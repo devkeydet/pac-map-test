cb69ed90c41af5c69996425a2fab634f95d37c47
Marc Schweigert <devkeydet@users.noreply.github.com>
2021-12-17 17:23:47 -0500

    init of working plugin build

---
+++ src/solutions/pacmaptest/plugins/pacmaptest.plugins/Plugin1.cs
@@ -0,0 +1,33 @@
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace pacmaptest.plugins
+{
+    /* 
+     * Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
+     * Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
+     */
+    public class Echo : PluginBase
+    {
+        public Echo(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(Echo))
+        {
+            // TODO: Implement your custom configuration handling
+            // https://docs.microsoft.com/powerapps/developer/common-data-service/register-plug-in#set-configuration-data
+        }
+
+        // Entry point for custom business logic execution
+        protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new ArgumentNullException(nameof(localPluginContext));
+            }
+
+            var context = localPluginContext.PluginExecutionContext;
+
+            string echoMe = (string)context.InputParameters["EchoMe"];
+            context.OutputParameters["EcoOutput"] = echoMe;
+        }
+    }
+}
